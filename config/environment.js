30340aade74bdc8d8196f5d88d2a7d518d229170
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-09 23:27:36 -0400

    gh pages

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/personal-portfolio/';
     // here you can enable a production-specific feature
   }
 
