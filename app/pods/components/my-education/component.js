0ebe593ab21d409735567b43b7c2ca96ec84da08
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-12 13:54:26 -0400

    style updates to experience

--- app/pods/components/my-education/component.js
+++ app/pods/components/my-education/component.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    educationExpand: false,
+    actions: {
+        toggleEducation: function() {
+            console.log(this.get('educationExpand'))
+            this.toggleProperty('educationExpand')
+        }
+    }
 });
