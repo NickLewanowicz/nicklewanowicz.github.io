86b26d240fe67db40ed84390428ed266028d20ce
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-18 22:52:55 -0400

    limited to 4 projects and tweaked css

--- app/pods/components/my-projects/component.js
+++ app/pods/components/my-projects/component.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    collapsed: false,
+
+    actions: {
+        toggleCollapsed() {
+            this.toggleProperty('collapsed')
+        }
+    }
 });
