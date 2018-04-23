f86e295657ce5bff30a0817197009790b96eaa1c
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-23 16:53:52 -0400

    Changelog: - Removing unused tests

--- tests/integration/pods/components/grid-card/component-test.js
+++ tests/integration/pods/components/grid-card/component-test.js
@@ -1,16 +1,16 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+// import { moduleForComponent, test } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('grid-card', 'Integration | Component | grid card', {
-  integration: true
-});
+// moduleForComponent('grid-card', 'Integration | Component | grid card', {
+//   integration: true
+// });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+// test('it renders', function(assert) {
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{grid-card}}`);
+//   this.render(hbs`{{grid-card}}`);
 
-  assert.equal(this.$().text().trim(), '');
+//   assert.equal(this.$().text().trim(), '');
 
-});
+// });
--- tests/integration/pods/components/my-recommendations/component-test.js
+++ tests/integration/pods/components/my-recommendations/component-test.js
@@ -1,24 +1,24 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+// import { moduleForComponent, test } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('my-recommendations', 'Integration | Component | my recommendations', {
-  integration: true
-});
+// moduleForComponent('my-recommendations', 'Integration | Component | my recommendations', {
+//   integration: true
+// });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+// test('it renders', function(assert) {
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{my-recommendations}}`);
+//   this.render(hbs`{{my-recommendations}}`);
 
-  assert.equal(this.$().text().trim(), '');
+//   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#my-recommendations}}
-      template block text
-    {{/my-recommendations}}
-  `);
+//   // Template block usage:
+//   this.render(hbs`
+//     {{#my-recommendations}}
+//       template block text
+//     {{/my-recommendations}}
+//   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+//   assert.equal(this.$().text().trim(), 'template block text');
+// });
